96618fb87d05d2ed6573b1711279fc2add886340
Szilágyi Csaba <ninjaraccoon4@gmail.com>
2020-04-12 18:39:41 +0200

    hazi

--- src/main/java/user/Main.java
+++ src/main/java/user/Main.java
@@ -1,4 +1,34 @@
 package user;
 
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:test");
+        jdbi.installPlugin(new SqlObjectPlugin());
+        try (Handle handle = jdbi.open()) {
+
+            UserDao dao = handle.attach(UserDao.class);
+            dao.createTable();
+            User user = User.builder()
+                    .name("James Bond")
+                    .username("007")
+                    .id(444L)
+                    .password("hello")
+                    .email("bond.james@citromail.hu")
+                    .gender(User.Gender.MALE)
+                    .dob(LocalDate.parse("1920-11-11"))
+                    .enabled(true)
+                    .build();
+            dao.insert(user);
+            System.out.println(dao.findById(user.getId()) + " : id-val");
+            System.out.println(dao.findByUsername(user.getUsername()) + ": username-mel");
+            dao.list().stream().forEach(System.out::println);
+            dao.delete(user);
+        }
+    }
 }
--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,4 +1,31 @@
 package user;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jdbi.v3.core.enums.EnumByOrdinal;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class User {
+    @EnumByOrdinal
+    public static enum Gender {
+        FEMALE,
+        MALE
+    }
+
+    private Long id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private Gender gender;
+    private LocalDate dob;
+    private boolean enabled;
 }
